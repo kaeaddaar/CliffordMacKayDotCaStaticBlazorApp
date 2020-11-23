c27d1d4a0f93b5c7a22b2dc9f0fa3f7505c4878b
Clifford Mackay <cliff@cmartme.com>
2020-11-22 16:25:42 -0800

    Revert "* Test - Migrate Production site"
    This reverts commit 0819af1745b36692961f84e31e920ca83af1f6e3.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,14 +1,10 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using BlazorApp.Shared;
-using Client;
+
 
 namespace BlazorApp.Client
 {
@@ -17,9 +13,11 @@ namespace BlazorApp.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
+            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
+            builder.Services.AddSingleton<CounterState>();
 
             await builder.Build().RunAsync();
         }
