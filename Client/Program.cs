0819af1745b36692961f84e31e920ca83af1f6e3
Clifford Mackay <cliff@cmartme.com>
2020-11-22 16:19:07 -0800

    * Test - Migrate Production site  - CliffordMacKayDotCaBlazor migrated  - Display issue not working so this is a test  - I will migrate from Client to CliffordMacKayDotCaBlazor as name to simplify these potential namespace issues. Then I will try again

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,14 @@
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using BlazorApp.Shared;
-
+using Client;
 
 namespace BlazorApp.Client
 {
@@ -13,11 +17,9 @@ namespace BlazorApp.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
-            var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
-            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
-            builder.Services.AddSingleton<CounterState>();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
